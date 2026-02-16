9f6cae53943156f60e67ddc2fb1253e8fda72097
monish394 <monishkimar951@gmail.com>
2026-02-16 10:57:46 +0530

    base url change to localhost

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance=axios.create({
-    // baseURL:"http://localhost:5000/api"
-    baseURL:"https://6992a9ea4cfd8890fe76a07a--visionary-gelato-527133.netlify.app/api"
+    baseURL:"http://localhost:5000/api"
+    // baseURL:"https://asset-maintenance-p7te.vercel.app/api"
 })
 
 export default axiosInstance
